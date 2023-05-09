95ba132793a9e6e0406f20a04efe6991bfc9fca0
Bindupoojary <bindupoojary09@gmail.com>
2023-05-09 07:51:17 +0530

    a

--- bindu_exercise/src/Controller/CustomController.php
+++ bindu_exercise/src/Controller/CustomController.php
@@ -6,14 +6,17 @@ use Drupal\Core\Controller\ControllerBase;        #This class serves as a base c
 
 class CustomController extends ControllerBase {    #class CustomController extends ControllerBase
 
-    public function hello() {
-        $hexcode = $this->configuration['hexcode'];
-        $element = [
-          '#theme' => "block_plugin_template",
-          '#text' =>  "welcome all",
-          '#hexcode' => $this->configuration['hexcode'],
+    public function hello() {                  #This is a public method named hello which does not take any arguments.
+      $route = \Drupal::service('bindu_exercise')->getName();
+      print_r($route);
+      //exit;
+        $hexcode = $this->configuration['hexcode'];   #his assigns the value of the hexcode  to a variable $hexcode.
+        $element = [                               #his creates an array called $element with three keys: #theme, #text, and #hexcode.
+          '#theme' => "block_plugin_template",        #The #theme key specifies the theme hook name,
+          '#text' =>  "welcome all",                  #text contains the text to be displayed
+          '#hexcode' => $this->configuration['hexcode'],  ##hexcode contains the value of $hexcode
         ];
-          return $element;
+          return $element;                      #return the array
 
     }
 

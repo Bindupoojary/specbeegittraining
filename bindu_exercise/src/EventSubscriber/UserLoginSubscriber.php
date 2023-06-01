ae3dc3c35d7950f5c379a48a162d4bf51206532e
Bindupoojary <bindupoojary09@gmail.com>
2023-06-01 15:00:31 +0530

    new

--- bindu_exercise/src/EventSubscriber/UserLoginSubscriber.php
+++ bindu_exercise/src/EventSubscriber/UserLoginSubscriber.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\bindu_exercise\EventSubscriber;
 
-use Drupal\custom_events\Event\UserLoginEvent;
+use Drupal\custom_events\Event\UserLoginEvent; // Import the UserLoginEvent class from the custom_events module.
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -16,9 +16,9 @@ class UserLoginSubscriber implements EventSubscriberInterface {
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
+    // Return an array mapping the event name to the method that should handle it.
     return [
-      // Static class constant => method on this class.
-      UserLoginEvent::EVENT_NAME => 'onUserLogin',
+      UserLoginEvent::EVENT_NAME => 'onUserLogin', // When UserLoginEvent is triggered, call the onUserLogin method.
     ];
   }
 
@@ -29,15 +29,18 @@ class UserLoginSubscriber implements EventSubscriberInterface {
    *   Our custom event object.
    */
   public function onUserLogin(UserLoginEvent $event) {
-    $database = \Drupal::database();
-    $dateFormatter = \Drupal::service('date.formatter');
+    // Perform actions when the user login event is triggered.
+    $database = \Drupal::database(); // Access the database service.
+    $dateFormatter = \Drupal::service('date.formatter'); // Access the date formatter service.
 
+    // Fetch the account creation date for the logged-in user.
     $account_created = $database->select('users_field_data', 'ud')
       ->fields('ud', ['created'])
       ->condition('ud.uid', $event->account->id())
       ->execute()
       ->fetchField();
 
+    // Display a status message with the formatted account creation date.
     \Drupal::messenger()->addStatus(t('Hello, your account was created on %created_date.', [
       '%created_date' => $dateFormatter->format($account_created, 'short'),
     ]));
--- dependent/src/Form/DependentForm.php
+++ dependent/src/Form/DependentForm.php
@@ -19,45 +19,57 @@ class DependentForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    // Retrieve the options for the first dropdown (category).
     $opt = $this->location();
+
+    // Retrieve the selected values from the form state, or set them to 'none' if not available.
     $cat = $form_state->getValue('category') ?: 'none';
     $avai = $form_state->getValue('availableitems') ?: 'none';
+
+    // Category dropdown.
     $form['category'] = [
-        '#type' => 'select',
-        '#title' => 'country',
-        '#options' => $opt,
-        'default_value' => $cat,
-        '#ajax' => [
-            'callback' => '::DropdownCallback',
-            'wrapper' => 'field-container',
-            'event' => 'change'
-        ]
+      '#type' => 'select',
+      '#title' => 'Country',
+      '#options' => $opt,
+      'default_value' => $cat,
+      '#ajax' => [
+        'callback' => '::DropdownCallback',  // AJAX callback method for this dropdown.
+        'wrapper' => 'field-container',  // HTML ID of the element to update via AJAX.
+        'event' => 'change',  // Trigger the AJAX callback on change event.
+      ],
     ];
+
+    // Available items dropdown.
     $form['availableitems'] = [
-        '#type' => 'select',
-        '#title' => 'state',
-        '#options' =>static::availableItems($cat),
-        '#default_value' => !empty($form_state->getValue('availableitems')) ? $form_state->getValue('availableitems') : 'none',
-        '#prefix' => '<div id="field-container"',
-        '#suffix' => '</div>',
-        '#ajax' => [
-          'callback' => '::DropdownCallback',
-          'wrapper' => 'dist-container',
-          'event' => 'change'
-      ]
+      '#type' => 'select',
+      '#title' => 'State',
+      '#options' => static::availableItems($cat),  // Generate options based on the selected category.
+      '#default_value' => !empty($form_state->getValue('availableitems')) ? $form_state->getValue('availableitems') : 'none',
+      '#prefix' => '<div id="field-container"',  // HTML prefix for the element.
+      '#suffix' => '</div>',  // HTML suffix for the element.
+      '#ajax' => [
+        'callback' => '::DropdownCallback',  // AJAX callback method for this dropdown.
+        'wrapper' => 'dist-container',  // HTML ID of the element to update via AJAX.
+        'event' => 'change',  // Trigger the AJAX callback on change event.
+      ],
     ];
+
+    // District dropdown.
     $form['district'] = [
-          '#type' => 'select',
-          '#title' => 'district',
-          '#options' =>static::district($avai),
-          '#default_value' => !empty($form_state->getValue('district')) ? $form_state->getValue('district') : '',
-          '#prefix' => '<div id="dist-container"',
-          '#suffix' => '</div>',
+      '#type' => 'select',
+      '#title' => 'District',
+      '#options' => static::district($avai),  // Generate options based on the selected available items.
+      '#default_value' => !empty($form_state->getValue('district')) ? $form_state->getValue('district') : '',
+      '#prefix' => '<div id="dist-container"',  // HTML prefix for the element.
+      '#suffix' => '</div>',  // HTML suffix for the element.
     ];
+
+    // Submit button.
     $form['submit'] = [
-        '#type' => 'submit',
-        '#value' => 'Submit',
+      '#type' => 'submit',
+      '#value' => 'Submit',
     ];
+
     return $form;
   }
 
@@ -66,66 +78,76 @@ class DependentForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $trigger = (string) $form_state->getTriggeringElement()['#value'];
+
+    // If the triggering element is not the submit button, set the form state to rebuild.
     if ($trigger != 'submit') {
-        $form_state->setRebuild();
+      $form_state->setRebuild();
     }
   }
 
+  /**
+   * AJAX callback method for the dependent dropdowns.
+   */
   public function DropdownCallback(array &$form, FormStateInterface $form_state) {
     $triggering_element = $form_state->getTriggeringElement();
     $triggering_element_name = $triggering_element['#name'];
 
     if ($triggering_element_name === 'category') {
+      // Return the available items dropdown to update via AJAX.
       return $form['availableitems'];
-    }
-    elseif ($triggering_element_name === 'availableitems') {
+    } elseif ($triggering_element_name === 'availableitems') {
+      // Return the district dropdown to update via AJAX.
       return $form['district'];
     }
-
-
   }
 
+  /**
+   * Get the options for the category dropdown.
+   */
   public function location() {
     return [
-        'none' => '-none-',
-        'india' => 'india',
+      'none' => '-none-',
+      'india' => 'India',
     ];
   }
 
+  /**
+   * Get the options for the available items dropdown based on the selected category.
+   */
   public function availableItems($cat) {
     switch ($cat) {
-        case 'india':
-            $opt = [
-                'karnataka' => 'karnataka',
-                'kerala' => 'kerala',
-            ];
+      case 'india':
+        $opt = [
+          'karnataka' => 'Karnataka',
+          'kerala' => 'Kerala',
+        ];
         break;
-        default:
-          $opt = ['none' => '-none-'];
+      default:
+        $opt = ['none' => '-none-'];
         break;
     }
     return $opt;
   }
 
+  /**
+   * Get the options for the district dropdown based on the selected available items.
+   */
   public function district($avai) {
-    switch($avai) {
+    switch ($avai) {
       case 'karnataka':
         $opt = [
-          'kodagu' => 'kodagu',
-          'bangalore' => 'bangalore',
-          'mangalore' => 'mangalore',
+          'kodagu' => 'Kodagu',
+          'bangalore' => 'Bangalore',
+          'mangalore' => 'Mangalore',
         ];
-      break;
+        break;
       case 'kerala':
         $opt = [
-          'kasargod' => 'kasargod',
-          'trivandrum' => 'trivandrum',
+          'kasargod' => 'Kasargod',
+          'trivandrum' => 'Trivandrum',
         ];
-      break;
+        break;
     }
     return $opt;
   }
-
-
-
 }
